bec2904dc886c553e4a7f4b9d4ff78c3ea38f8b5
Henrik Gharagyozyan <henrik.gharagyozyan1@gmail.com>
2025-09-25 14:41:42 +0400

    Dispatching events from GLFW.

--- Hydra/src/Hydra/Application.cpp
+++ Hydra/src/Hydra/Application.cpp
@@ -8,15 +8,27 @@
 
 namespace Hydra
 {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
-	}
+	} 
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		HD_CORE_TRACE("{0}", e);
+	}
+ 
 	void Application::Run()
 	{
 		while (m_Running)
@@ -25,5 +37,11 @@ namespace Hydra
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
