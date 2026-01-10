1277150d088682fc274e6e6cc3b6edfbf01e9469
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2026-01-10 21:58:41 +0400

    Changed macro name

--- Hydra/src/Hydra/Log.h
+++ Hydra/src/Hydra/Log.h
@@ -21,15 +21,15 @@ namespace Hydra
 }
 
 // Core log macros
-#define HD_CORE_ERROR(...)  ::Hydra::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define HD_CORE_WARN(...)   ::Hydra::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define HD_CORE_INFO(...)   ::Hydra::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define HD_CORE_TRACE(...)  ::Hydra::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define HD_CORE_FATAL(...)  ::Hydra::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define HD_CORE_ERROR(...)     ::Hydra::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define HD_CORE_WARN(...)      ::Hydra::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define HD_CORE_INFO(...)      ::Hydra::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define HD_CORE_TRACE(...)     ::Hydra::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define HD_CORE_CRITICAL(...)  ::Hydra::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
-#define HD_ERROR(...)		::Hydra::Log::GetClientLogger()->error(__VA_ARGS__)
-#define HD_WARN(...)		::Hydra::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define HD_INFO(...)		::Hydra::Log::GetClientLogger()->info(__VA_ARGS__)
-#define HD_TRACE(...)		::Hydra::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define HD_FATAL(...)		::Hydra::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define HD_ERROR(...)		   ::Hydra::Log::GetClientLogger()->error(__VA_ARGS__)
+#define HD_WARN(...)		   ::Hydra::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define HD_INFO(...)		   ::Hydra::Log::GetClientLogger()->info(__VA_ARGS__)
+#define HD_TRACE(...)		   ::Hydra::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HD_CRITICAL(...)	   ::Hydra::Log::GetClientLogger()->critical(__VA_ARGS__)
