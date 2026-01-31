507a60d81aaa776bb67c36dc9014ecc3e0560fae
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2026-01-31 20:28:55 +0400

    Adding flag for MultiProcessor Compilation in Premake

--- premake5.lua
+++ premake5.lua
@@ -9,6 +9,11 @@ workspace "Hydra"
 		"Dist"
 	}
 
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
