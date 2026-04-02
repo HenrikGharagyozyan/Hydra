e2361c3b169e66a3fd6fe781a4b3836c6db58025
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2026-04-02 21:52:10 +0400

    Added check for OpenGL version

--- Hydra/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hydra/src/Platform/OpenGL/OpenGLContext.cpp
@@ -27,7 +27,16 @@ namespace Hydra
 		HD_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
 		HD_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
 		HD_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+		#ifdef HD_ENABLE_ASSERTS
+			int versionMajor;
+			int versionMinor;
+			glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+			glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+			HD_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Hydra requires at least OpenGL version 4.5!");
+		#endif
 	}
+	
 	void OpenGLContext::SwapBuffers()
 	{
 		glfwSwapBuffers(m_WindowHandle);
