8a865a4446ffa1b4415f20631bdda10264856c5b
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2026-04-10 18:40:55 +0400

    Fix Build warnings to do with BufferElement Offset Type

--- Hydra/src/Hydra/Renderer/Buffer.h
+++ Hydra/src/Hydra/Renderer/Buffer.h
@@ -11,16 +11,9 @@ namespace Hydra
     enum class ShaderDataType
     {
         None = 0,
-        Float,
-        Float2,
-        Float3,
-        Float4,
-        Mat3,
-        Mat4,
-        Int,
-        Int2,
-        Int3,
-        Int4,
+        Float, Float2, Float3, Float4,
+        Mat3, Mat4,
+        Int, Int2, Int3, Int4,
         Bool
     };
 
@@ -28,28 +21,17 @@ namespace Hydra
     {
         switch (type)
         {
-        case ShaderDataType::Float:
-            return 4;
-        case ShaderDataType::Float2:
-            return 4 * 2;
-        case ShaderDataType::Float3:
-            return 4 * 3;
-        case ShaderDataType::Float4:
-            return 4 * 4;
-        case ShaderDataType::Mat3:
-            return 4 * 3 * 3;
-        case ShaderDataType::Mat4:
-            return 4 * 4 * 4;
-        case ShaderDataType::Int:
-            return 4;
-        case ShaderDataType::Int2:
-            return 4 * 2;
-        case ShaderDataType::Int3:
-            return 4 * 3;
-        case ShaderDataType::Int4:
-            return 4 * 4;
-        case ShaderDataType::Bool:
-            return 1;
+            case ShaderDataType::Float:   return 4;
+            case ShaderDataType::Float2:  return 4 * 2;
+            case ShaderDataType::Float3:  return 4 * 3;
+            case ShaderDataType::Float4:  return 4 * 4;
+            case ShaderDataType::Mat3:    return 4 * 3 * 3;
+            case ShaderDataType::Mat4:    return 4 * 4 * 4;
+            case ShaderDataType::Int:     return 4;
+            case ShaderDataType::Int2:    return 4 * 2;
+            case ShaderDataType::Int3:    return 4 * 3;
+            case ShaderDataType::Int4:    return 4 * 4;
+            case ShaderDataType::Bool:    return 1;
         }
 
         HD_CORE_ASSERT(false, "Unknown ShaderDataType!");
@@ -64,10 +46,14 @@ namespace Hydra
         uint32_t Offset;
         bool Normalized;
 
-        BufferElement() {}
+        BufferElement() = default;
 
-        BufferElement(ShaderDataType type, const std::string &name, bool normalized = false)
-            : Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
+        BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
+            : Name(name)
+            , Type(type)
+            , Size(ShaderDataTypeSize(type))
+            , Offset(0)
+            , Normalized(normalized)
         {
         }
 
@@ -75,28 +61,17 @@ namespace Hydra
         {
             switch (Type)
             {
-            case ShaderDataType::Float:
-                return 1;
-            case ShaderDataType::Float2:
-                return 2;
-            case ShaderDataType::Float3:
-                return 3;
-            case ShaderDataType::Float4:
-                return 4;
-            case ShaderDataType::Mat3:
-                return 3 * 3;
-            case ShaderDataType::Mat4:
-                return 4 * 4;
-            case ShaderDataType::Int:
-                return 1;
-            case ShaderDataType::Int2:
-                return 2;
-            case ShaderDataType::Int3:
-                return 3;
-            case ShaderDataType::Int4:
-                return 4;
-            case ShaderDataType::Bool:
-                return 1;
+                case ShaderDataType::Float:   return 1;
+                case ShaderDataType::Float2:  return 2;
+                case ShaderDataType::Float3:  return 3;
+                case ShaderDataType::Float4:  return 4;
+                case ShaderDataType::Mat3:    return 3 * 3;
+                case ShaderDataType::Mat4:    return 4 * 4;
+                case ShaderDataType::Int:     return 1;
+                case ShaderDataType::Int2:    return 2;
+                case ShaderDataType::Int3:    return 3;
+                case ShaderDataType::Int4:    return 4;
+                case ShaderDataType::Bool:    return 1;
             }
 
             HD_CORE_ASSERT(false, "Unknown ShaderDataType!");
@@ -109,26 +84,26 @@ namespace Hydra
     public:
         BufferLayout() {}
 
-        BufferLayout(const std::initializer_list<BufferElement> &elements)
+        BufferLayout(const std::initializer_list<BufferElement>& elements)
             : m_Elements(elements)
         {
             CalculateOffsetsAndStride();
         }
 
         inline uint32_t GetStride() const { return m_Stride; }
-        inline const std::vector<BufferElement> &GetElements() const { return m_Elements; }
+        inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
 
-        std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
-        std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
-        std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
-        std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
+        std::vector<BufferElement>::iterator begin()              { return m_Elements.begin(); }
+        std::vector<BufferElement>::iterator end()                { return m_Elements.end(); }
+        std::vector<BufferElement>::const_iterator begin() const  { return m_Elements.begin(); }
+        std::vector<BufferElement>::const_iterator end() const    { return m_Elements.end(); }
 
     private:
         void CalculateOffsetsAndStride()
         {
-            uint32_t offset = 0;
+            size_t offset = 0;
             m_Stride = 0;
-            for (auto &element : m_Elements)
+            for (auto& element : m_Elements)
             {
                 element.Offset = offset;
                 offset += element.Size;
@@ -152,7 +127,7 @@ namespace Hydra
         virtual const BufferLayout &GetLayout() const = 0;
         virtual void SetLayout(const BufferLayout &layout) = 0;
 
-        static VertexBuffer *Create(float *vertices, uint32_t size);
+        static VertexBuffer *Create(float* vertices, uint32_t size);
     };
 
     class IndexBuffer
@@ -165,7 +140,7 @@ namespace Hydra
 
         virtual uint32_t GetCount() const = 0;
 
-        static IndexBuffer *Create(uint32_t *indices, uint32_t count);
+        static IndexBuffer* Create(uint32_t* indices, uint32_t count);
     };
 
 }
