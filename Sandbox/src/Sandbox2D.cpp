f042c70b4318766ed292f4dc2f7a3d87ced2035f
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2026-04-10 19:46:57 +0400

    Added transforms to Renderer2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -5,8 +5,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "Platform/OpenGL/OpenGLShader.h"
-
 
 Sandbox2D::Sandbox2D()
     : Layer("Sandbox2D")
@@ -34,11 +32,8 @@ void Sandbox2D::OnUpdate(Hydra::Timestep ts)
 
     Hydra::Renderer2D::BeginScene(m_CameraController.GetCamera());
     Hydra::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+    Hydra::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
     Hydra::Renderer2D::EndScene();
-
-    // TODO:  Add these functions - Shader::SetMat4, Shader::SetFloat4
-    // std::dynamic_pointer_cast<Hydra::OpenGLShader>(m_FlatColorShader)->Bind();
-    // std::dynamic_pointer_cast<Hydra::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
 void Sandbox2D::OnImGuiRender()
