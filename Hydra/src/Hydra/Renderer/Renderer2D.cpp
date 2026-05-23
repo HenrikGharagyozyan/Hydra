6c8974a0a57195457c6f179a2c871b997590a73c
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2026-05-23 23:59:21 +0400

    Fixed the ImGui-controlled square color

--- Hydra/src/Hydra/Renderer/Renderer2D.cpp
+++ Hydra/src/Hydra/Renderer/Renderer2D.cpp
@@ -52,11 +52,13 @@ namespace Hydra
         s_Data.QuadVertexArray = VertexArray::Create();
 
         s_Data.QuadVertexBuffer = VertexBuffer::Create(s_Data.MaxVertices * sizeof(QuadVertex));
-        s_Data.QuadVertexBuffer->SetLayout({{ShaderDataType::Float3, "a_Position"},
-                                            {ShaderDataType::Float4, "a_Color"},
-                                            {ShaderDataType::Float2, "a_TexCoord"},
-                                            {ShaderDataType::Float, "a_TexIndex"},
-                                            {ShaderDataType::Float, "a_TilingFactor"}});
+        s_Data.QuadVertexBuffer->SetLayout({
+                { ShaderDataType::Float3, "a_Position" },
+                { ShaderDataType::Float4, "a_Color" },
+                { ShaderDataType::Float2, "a_TexCoord" },
+                { ShaderDataType::Float, "a_TexIndex" },
+                { ShaderDataType::Float, "a_TilingFactor" }
+            });
         s_Data.QuadVertexArray->AddVertexBuffer(s_Data.QuadVertexBuffer);
 
         s_Data.QuadVertexBufferBase = new QuadVertex[s_Data.MaxVertices];
@@ -94,10 +96,10 @@ namespace Hydra
 
         s_Data.TextureSlots[0] = s_Data.WhiteTexture;
 
-        s_Data.QuadVertexPositions[0] = {-0.5f, -0.5f, 0.0f, 1.0f};
-        s_Data.QuadVertexPositions[1] = {0.5f, -0.5f, 0.0f, 1.0f};
-        s_Data.QuadVertexPositions[2] = {0.5f, 0.5f, 0.0f, 1.0f};
-        s_Data.QuadVertexPositions[3] = {-0.5f, 0.5f, 0.0f, 1.0f};
+        s_Data.QuadVertexPositions[0] = { -0.5f, -0.5f, 0.0f, 1.0f };
+        s_Data.QuadVertexPositions[1] = { 0.5f, -0.5f, 0.0f, 1.0f };
+        s_Data.QuadVertexPositions[2] = { 0.5f, 0.5f, 0.0f, 1.0f };
+        s_Data.QuadVertexPositions[3] = { -0.5f, 0.5f, 0.0f, 1.0f };
     }
 
     void Renderer2D::Shutdown()
@@ -105,7 +107,7 @@ namespace Hydra
         HD_PROFILE_FUNCTION();
     }
 
-    void Renderer2D::BeginScene(const OrthographicCamera &camera)
+    void Renderer2D::BeginScene(const OrthographicCamera& camera)
     {
         HD_PROFILE_FUNCTION();
 
@@ -150,12 +152,12 @@ namespace Hydra
         s_Data.TextureSlotIndex = 1;
     }
 
-    void Renderer2D::DrawQuad(const glm::vec2 &position, const glm::vec2 &size, const glm::vec4 &color)
+    void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
     {
         DrawQuad({position.x, position.y, 0.0f}, size, color);
     }
 
-    void Renderer2D::DrawQuad(const glm::vec3 &position, const glm::vec2 &size, const glm::vec4 &color)
+    void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
     {
         HD_PROFILE_FUNCTION();
 
@@ -165,7 +167,8 @@ namespace Hydra
         const float textureIndex = 0.0f; // White Texture
         const float tilingFactor = 1.0f;
 
-        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1});
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) 
+                                    * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1});
 
         s_Data.QuadVertexBufferPtr->Position = transform * s_Data.QuadVertexPositions[0];
         s_Data.QuadVertexBufferPtr->Color = color;
@@ -200,12 +203,12 @@ namespace Hydra
         s_Data.Stats.QuadCount++;
     }
 
-    void Renderer2D::DrawQuad(const glm::vec2 &position, const glm::vec2 &size, const Ref<Texture2D> &texture, float tilingFactor, const glm::vec4 &tintColor)
+    void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
     {
         DrawQuad({position.x, position.y, 0.0f}, size, texture, tilingFactor, tintColor);
     }
 
-    void Renderer2D::DrawQuad(const glm::vec3 &position, const glm::vec2 &size, const Ref<Texture2D> &texture, float tilingFactor, const glm::vec4 &tintColor)
+    void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
     {
         HD_PROFILE_FUNCTION();
 
@@ -232,7 +235,8 @@ namespace Hydra
             ++s_Data.TextureSlotIndex;
         }
 
-        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1});
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) 
+                                    * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1});
 
         s_Data.QuadVertexBufferPtr->Position = transform * s_Data.QuadVertexPositions[0];
         s_Data.QuadVertexBufferPtr->Color = color;
@@ -267,12 +271,12 @@ namespace Hydra
         s_Data.Stats.QuadCount++;
     }
 
-    void Renderer2D::DrawRotatedQuad(const glm::vec2 &position, const glm::vec2 &size, float rotation, const glm::vec4 &color)
+    void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
     {
         DrawRotatedQuad({position.x, position.y, 0.0f}, size, rotation, color);
     }
 
-    void Renderer2D::DrawRotatedQuad(const glm::vec3 &position, const glm::vec2 &size, float rotation, const glm::vec4 &color)
+    void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color)
     {
         HD_PROFILE_FUNCTION();
 
@@ -282,7 +286,9 @@ namespace Hydra
         const float textureIndex = 0.0f; // White Texture
         const float tilingFactor = 1.0f;
 
-        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::rotate(glm::mat4(1.0f), glm::radians(rotation), {0.0f, 0.0f, 1.0f}) * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1});
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) 
+                                    * glm::rotate(glm::mat4(1.0f), glm::radians(rotation), {0.0f, 0.0f, 1.0f}) 
+                                    * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1});
 
         s_Data.QuadVertexBufferPtr->Position = transform * s_Data.QuadVertexPositions[0];
         s_Data.QuadVertexBufferPtr->Color = color;
@@ -317,12 +323,12 @@ namespace Hydra
         s_Data.Stats.QuadCount++;
     }
 
-    void Renderer2D::DrawRotatedQuad(const glm::vec2 &position, const glm::vec2 &size, float rotation, const Ref<Texture2D> &texture, float tilingFactor, const glm::vec4 &tintColor)
+    void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
     {
         DrawRotatedQuad({position.x, position.y, 0.0f}, size, rotation, texture, tilingFactor, tintColor);
     }
 
-    void Renderer2D::DrawRotatedQuad(const glm::vec3 &position, const glm::vec2 &size, float rotation, const Ref<Texture2D> &texture, float tilingFactor, const glm::vec4 &tintColor)
+    void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
     {
         HD_PROFILE_FUNCTION();
 
@@ -349,7 +355,9 @@ namespace Hydra
             ++s_Data.TextureSlotIndex;
         }
 
-        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::rotate(glm::mat4(1.0f), glm::radians(rotation), {0.0f, 0.0f, 1.0f}) * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1});
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) 
+                                    * glm::rotate(glm::mat4(1.0f), glm::radians(rotation), {0.0f, 0.0f, 1.0f}) 
+                                    * glm::scale(glm::mat4(1.0f), {size.x, size.y, 1});
 
         s_Data.QuadVertexBufferPtr->Position = transform * s_Data.QuadVertexPositions[0];
         s_Data.QuadVertexBufferPtr->Color = color;
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -11,6 +11,7 @@
 Sandbox2D::Sandbox2D()
     : Layer("Sandbox2D")
     , m_CameraController(1280.0f / 720.0f, true)
+    , m_SquareColor({ 0.2f, 0.3f, 0.8f, 1.0f })
 {
 }
 
@@ -50,7 +51,7 @@ void Sandbox2D::OnUpdate(Hydra::Timestep ts)
         Hydra::Renderer2D::BeginScene(m_CameraController.GetCamera());
         Hydra::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, 45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
         Hydra::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-        Hydra::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
+        Hydra::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, m_SquareColor);
         Hydra::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
         Hydra::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
         Hydra::Renderer2D::EndScene();
