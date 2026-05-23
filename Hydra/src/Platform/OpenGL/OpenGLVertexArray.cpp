65b5cd41675cc4fb6c74175534bbc703c4d193a7
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2026-05-24 00:38:41 +0400

    Fix int-to-pointer cast warning in OpenGLVertexArray

--- Hydra/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Hydra/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -96,7 +96,7 @@ namespace Hydra
 						ShaderDataTypeToOpenGLBaseType(element.Type),
 						element.Normalized ? GL_TRUE : GL_FALSE,
 						layout.GetStride(),
-						(const void*)element.Offset
+						reinterpret_cast<const void*>(static_cast<uintptr_t>(element.Offset))
                     );
 					++m_VertexBufferIndex;
 					break;
@@ -137,5 +137,5 @@ namespace Hydra
 
         m_IndexBuffer = indexBuffer;
     }
-    
+
 }
