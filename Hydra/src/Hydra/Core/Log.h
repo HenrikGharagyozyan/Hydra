fc6382eb4c98d4834ead34e46c21a65c1b272c1b
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-06-07 14:23:55 +0400

    build(logging): hide compiler warnings from spdlog

--- Hydra/src/Hydra/Core/Log.h
+++ Hydra/src/Hydra/Core/Log.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include "Base.h"
+
+// This ignores all warnings raised inside External headers
+#pragma warning(push, 0)
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
+#pragma warning(pop)
+
 
 namespace Hydra
 {
