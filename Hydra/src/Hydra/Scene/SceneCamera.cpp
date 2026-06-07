9c4109eefa55476cb7d9162db5db04d7c1c32e53
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-06-07 14:04:29 +0400

    refactor(camera): fix GUI variable naming and standardize function ordering
    - Fix inconsistent variable names for Perspective Camera in the inspector UI
    - Reorder methods in SceneCamera to maintain a consistent lifecycle and declaration order

--- Hydra/src/Hydra/Scene/SceneCamera.cpp
+++ Hydra/src/Hydra/Scene/SceneCamera.cpp
@@ -11,21 +11,21 @@ namespace Hydra
         RecalculateProjection();
     }
 
-    void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
-    {
-        m_ProjectionType = ProjectionType::Orthographic;
-        m_OrthographicSize = size;
-        m_OrthographicNearClip = nearClip;
-        m_OrthographicFarClip = farClip;
-        RecalculateProjection();
-    }
-
     void SceneCamera::SetPerspective(float verticalFOV, float nearClip, float farClip)
     {
         m_ProjectionType = ProjectionType::Perspective;
         m_PerspectiveFOV = verticalFOV;
         m_PerspectiveNear = nearClip;
         m_PerspectiveFar = farClip;
+        RecalculateProjection();
+    }
+
+    void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
+    {
+        m_ProjectionType = ProjectionType::Orthographic;
+        m_OrthographicSize = size;
+        m_OrthographicNearClip = nearClip;
+        m_OrthographicFarClip = farClip;
         RecalculateProjection();
     }
 
