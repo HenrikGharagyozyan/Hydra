9df5b25e2defb79939a7cd14a350c3e04286c592
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-06-07 23:16:54 +0400

    refactor(renderer): introduce StartBatch and rename FlushAndReset to NextBatch

--- Hydra/src/Hydra/Renderer/Renderer2D.cpp
+++ Hydra/src/Hydra/Renderer/Renderer2D.cpp
@@ -117,10 +117,7 @@ namespace Hydra
         s_Data.TextureShader->Bind();
         s_Data.TextureShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
 
-        s_Data.QuadIndexCount = 0;
-        s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
-
-        s_Data.TextureSlotIndex = 1;
+        StartBatch();
     }
 
     void Renderer2D::BeginScene(const Camera& camera, const glm::mat4& transform)
@@ -132,10 +129,7 @@ namespace Hydra
         s_Data.TextureShader->Bind();
         s_Data.TextureShader->SetMat4("u_ViewProjection", viewProj);
 
-        s_Data.QuadIndexCount = 0;
-        s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
-
-        s_Data.TextureSlotIndex = 1;
+        StartBatch();
     }
 
 
@@ -143,16 +137,24 @@ namespace Hydra
     {
         HD_PROFILE_FUNCTION();
 
-        uint32_t dataSize = (uint32_t)((uint8_t*)s_Data.QuadVertexBufferPtr - (uint8_t*)s_Data.QuadVertexBufferBase);
-        s_Data.QuadVertexBuffer->SetData(s_Data.QuadVertexBufferBase, dataSize);
-
         Flush();
+    }
+
+    void Renderer2D::StartBatch()
+    {
+        s_Data.QuadIndexCount = 0;
+		s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
+		s_Data.TextureSlotIndex = 1;
     }
 
     void Renderer2D::Flush()
     {
         if (s_Data.QuadIndexCount == 0)
             return; // Nothing to draw
+
+        uint32_t dataSize = (uint32_t)((uint8_t*)s_Data.QuadVertexBufferPtr - (uint8_t*)s_Data.QuadVertexBufferBase);
+		s_Data.QuadVertexBuffer->SetData(s_Data.QuadVertexBufferBase, dataSize);
             
         // Bind textures
         for (uint32_t i = 0; i < s_Data.TextureSlotIndex; ++i)
@@ -164,14 +166,10 @@ namespace Hydra
         s_Data.Stats.DrawCalls++;
     }
 
-    void Renderer2D::FlushAndReset()
+    void Renderer2D::NextBatch()
     {
-        EndScene();
-
-        s_Data.QuadIndexCount = 0;
-        s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
-
-        s_Data.TextureSlotIndex = 1;
+        Flush();
+		StartBatch();
     }
 
     void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
@@ -214,7 +212,7 @@ namespace Hydra
         const float tilingFactor = 1.0f;
 
         if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
-            FlushAndReset();
+            NextBatch();
 
         for (size_t i = 0; i < quadVertexCount; ++i)
         {
@@ -236,10 +234,10 @@ namespace Hydra
         HD_PROFILE_FUNCTION();
 
         constexpr size_t quadVertexCount = 4;
-        constexpr glm::vec2 textureCoords[] = { {0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 1.0f} };
+        constexpr glm::vec2 textureCoords[] = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.0f, 1.0f } };
 
         if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
-            FlushAndReset();
+            NextBatch();
 
         float textureIndex = 0.0f;
 
@@ -255,7 +253,7 @@ namespace Hydra
         if (textureIndex == 0.0f)
         {
             if (s_Data.TextureSlotIndex >= Renderer2DData::MaxTextureSlots)
-				FlushAndReset();
+				NextBatch();
 
             textureIndex = (float)s_Data.TextureSlotIndex;
             s_Data.TextureSlots[s_Data.TextureSlotIndex] = texture;
--- Hydra/src/Hydra/Renderer/Renderer2D.h
+++ Hydra/src/Hydra/Renderer/Renderer2D.h
@@ -48,7 +48,8 @@ namespace Hydra
         static Statistics GetStats();
 
     private:
-        static void FlushAndReset();
+        static void StartBatch();
+		static void NextBatch();
     };
 
 }
