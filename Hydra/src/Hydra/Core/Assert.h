e616a461493c91b29d3a529472e92b365bb25864
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-06-07 23:07:55 +0400

    refactor(core): replace custom filename extraction with std::filesystem

--- Hydra/src/Hydra/Core/Assert.h
+++ Hydra/src/Hydra/Core/Assert.h
@@ -3,28 +3,14 @@
 #include "Hydra/Core/Base.h"
 #include "Hydra/Core/Log.h"
 
-#ifdef HD_ENABLE_ASSERTS
+#include <filesystem>
 
-    namespace Hydra::Assert
-    {
-        // Возвращает только имя файла без полного пути для чистоты логов
-        constexpr const char* CurrentFileName(const char* path) 
-        {
-            const char* file = path;
-            while (*path)
-            {
-                if (*path == '/' || *path == '\\')
-                    file = ++path;
-                else
-                    path++;
-            }
-            return file;
-        }
-    }
+
+#ifdef HD_ENABLE_ASSERTS
 
     #define HD_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { HD##type##ERROR(msg, __VA_ARGS__); HD_DEBUGBREAK(); } }
     #define HD_INTERNAL_ASSERT_WITH_MSG(type, check, ...) HD_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-    #define HD_INTERNAL_ASSERT_NO_MSG(type, check) HD_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", HD_STRINGIFY_MACRO(check), ::Hydra::Assert::CurrentFileName(__FILE__), __LINE__)
+    #define HD_INTERNAL_ASSERT_NO_MSG(type, check) HD_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", HD_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
     #define HD_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
     #define HD_INTERNAL_ASSERT_GET_MACRO(...) HD_EXPAND_MACRO( HD_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, HD_INTERNAL_ASSERT_WITH_MSG, HD_INTERNAL_ASSERT_NO_MSG) )
