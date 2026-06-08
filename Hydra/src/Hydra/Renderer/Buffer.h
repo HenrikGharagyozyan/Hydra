188e490625fed3ab06a3c3c5082eefe7a28de489
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-06-08 12:49:54 +0400

    refactor: apply miscellaneous code cleanups and micro-optimizations
    - Pass std::initializer_list by value in BufferLayout constructor for efficiency
    - Enforce const-correctness for Renderer2D vertex and index count getters
    - Modernize VertexArray by explicitly defaulting its virtual destructor

--- Hydra/src/Hydra/Renderer/Buffer.h
+++ Hydra/src/Hydra/Renderer/Buffer.h
@@ -84,7 +84,7 @@ namespace Hydra
     public:
         BufferLayout() {}
 
-        BufferLayout(const std::initializer_list<BufferElement>& elements)
+        BufferLayout(std::initializer_list<BufferElement> elements)
             : m_Elements(elements)
         {
             CalculateOffsetsAndStride();
