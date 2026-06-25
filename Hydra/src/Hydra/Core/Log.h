649439e0c33081e1a16ea506cce0a524a8d529e3
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-06-25 22:04:36 +0400

    debug: enable logging for glm vectors, matrices, and quaternions

--- Hydra/src/Hydra/Core/Log.h
+++ Hydra/src/Hydra/Core/Log.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/gtx/string_cast.hpp>
+
 #include "Base.h"
 
 // This ignores all warnings raised inside External headers
@@ -25,6 +29,24 @@ namespace Hydra
 		static Ref<spdlog::logger> s_ClientLogger;
 	};
 
+}
+
+template<typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::vec<L, T, Q>& vector)
+{
+	return os << glm::to_string(vector);
+}
+
+template<typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
+{
+	return os << glm::to_string(matrix);
+}
+
+template<typename OStream, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternio)
+{
+	return os << glm::to_string(quaternio);
 }
 
 // Core log macros
